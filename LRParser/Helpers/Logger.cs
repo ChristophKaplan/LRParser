c6de05fbdaa707e79a78d6b6ebbc6feb00466989
Christoph Kaplan <contact@christophkaplan.de>
2025-01-02 13:28:32 +0100

    guard

--- LRParser/Helpers/Logger.cs
+++ LRParser/Helpers/Logger.cs
@@ -2,6 +2,10 @@ namespace Helpers;
 
 public class Logger {
     public static void Log(string message) {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        UnityEngine.Debug.Log(message);
+#else
         Console.WriteLine(message);
+#endif
     }
 }
